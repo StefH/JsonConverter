f4e19db3e34244363bb6e6d07569c271e819ec4d
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-08-14 14:02:49 +0200

    JsonConverter.NetJSON

---
+++ src/JsonConverter.NetJSON/JsonConverterAsync.cs
@@ -0,0 +1,31 @@
+﻿#if !(NET35 || NET40)
+using JsonConverter.Abstractions;
+using Stef.Validation;
+
+namespace JsonConverter.NetJSON;
+
+public partial class JsonConverter
+{
+    public Task<T?> DeserializeAsync<T>(string text, IJsonConverterOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(Deserialize<T>(text, options));
+    }
+
+    public Task<T?> DeserializeAsync<T>(Stream stream, IJsonConverterOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(Deserialize<T>(stream, options));
+    }
+
+    public Task<string> SerializeAsync<T>(T source, IJsonConverterOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(Serialize(source, options));
+    }
+
+    public async Task<bool> IsValidJsonAsync(Stream stream, CancellationToken cancellationToken = default)
+    {
+        Guard.NotNull(stream);
+
+        return IsValidJson(await stream.ReadAsStringAsync().ConfigureAwait(false));
+    }
+}
+#endif
